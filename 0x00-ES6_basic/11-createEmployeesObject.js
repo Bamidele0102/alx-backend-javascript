2914ca688d110511e87e37ce18096ba993b881e6
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-06-24 23:03:50 +0100

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
-    [departmentName]: employees,
+  const employment = {
+    [departmentName]: [
+      ...employees,
+    ],
   };
+  return employment;
 }
