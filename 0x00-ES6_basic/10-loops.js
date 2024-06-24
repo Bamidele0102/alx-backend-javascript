2ade9f6fca903726863235a355870320c16bb37d
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-06-24 22:33:52 +0100

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const appendedArray = [];
-    for (let item of array) {
-      item = appendString + item;
-      appendedArray.push(item);
-    }
-  
-    return appendedArray;
+  const appendedArray = [];
+  for (let item of array) {
+    item = appendString + item;
+    appendedArray.push(item);
   }
+
+  return appendedArray;
+}
