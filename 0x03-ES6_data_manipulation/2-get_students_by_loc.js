325d119afb5aa08e96dad6645865dab68a260099
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-02 13:05:11 +0100

    Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  if (Array.isArray(students)) {
-    return students.filter((student) => student.location === city);
-  }
-  return [];
+  return students.filter((student) => student.location === city);
 }
