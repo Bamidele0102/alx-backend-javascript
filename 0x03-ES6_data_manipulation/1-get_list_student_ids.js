34f4adbc39ee3a6ae5fc81161f53c4784204497b
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-02 12:58:13 +0100

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) return [];
-  return students.map((student) => student.id);
+  if (!Array.isArray(students)) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
