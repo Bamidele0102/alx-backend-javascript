ae3755d9f4e99f9dca97ea57cf9ee18418487751
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-20 22:34:10 +0100

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,12 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Idowu Olayiwola Bamidele <https://github.com/Bamidele0102>
+ */
 class AppController {
-  static getHomepage(_request, response) {
-    response.send(200, 'Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
 export default AppController;
+module.exports = AppController;
