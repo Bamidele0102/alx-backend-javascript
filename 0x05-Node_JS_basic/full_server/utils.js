5d9781365753b74e45bc246ba0e0cba43542f545
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-20 22:01:34 +0100

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,31 +1,40 @@
 const fs = require('fs');
 
-async function countStudents(path) {
-  let data;
-  try {
-    data = await fs.promises.readFile(path, 'utf8');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-  const students = data.split('\r\n').slice(1)
-    .map((student) => student.split(','))
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-  const fields = students.map((student) => student.field);
-  const uniqueFields = new Set(fields);
-  const studentsByField = {};
-  for (const field of uniqueFields) {
-    studentsByField[field] = [];
-  }
-  for (const student of students) {
-    studentsByField[student.field].push(student.firstName);
-  }
-  console.log(studentsByField);
-  return studentsByField;
-}
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (error, csvData) => {
+    if (error) {
+      reject(Error('Cannot load the database'));
+    }
+    if (csvData) {
+      const fields = {};
+      const dataShow = {};
+      let data = csvData.toString().split('\n');
+      data = data.filter((element) => element.length > 0);
 
-module.exports = countStudents;
+      data.shift();
+      data.forEach((element) => {
+        if (element.length > 0) {
+          const row = element.split(',');
+          if (row[3] in fields) {
+            fields[row[3]].push(row[0]);
+          } else {
+            fields[row[3]] = [row[0]];
+          }
+        }
+      });
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          dataShow[field] = {
+            list: `List: ${list.toString().replace(/,/g, ', ')}`,
+            number: list.length,
+          };
+        }
+      }
+
+      resolve(dataShow);
+    }
+  });
+});
+
+module.exports = readDatabase;
