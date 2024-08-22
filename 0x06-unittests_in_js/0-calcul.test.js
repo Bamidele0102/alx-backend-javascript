81c652dece4d915716ddd585df2c17535be3acbc
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-22 11:20:17 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,25 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
-  it('should return 4 when inputs are 1 and 3', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+describe('calculateNumber', () => {
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
 
-  it('should return 5 when inputs are 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  it('should return 5 when inputs are 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
